b337528ec644cd9180fb7021c8bc4fed80fe32d4
Idanord <33676867+Idanord@users.noreply.github.com>
2018-05-29 01:36:28 -0700

    Empty Project
    This is an empty project at this stage for the initial commit

---
+++ app/src/main/java/com/example/will/displaycontacts/DisplayContactsActivity.java
@@ -0,0 +1,13 @@
+package com.example.will.displaycontacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayContactsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_contacts);
+    }
+}
